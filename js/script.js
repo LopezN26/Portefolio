bbf23049f8dd5d7143f415fc393b321c2b78dec8
LopezN26 <n.lopez@it-students.fr>
2020-03-05 22:26:56 +0100

    a=add animations

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ var projects =$("#projects")[0].offsetTop;
 
 
 //conditions pour affichage du Background ou non dans le header
-$( window ).scroll(function() {
+$(window).scroll(function() {
     if ($(window).scrollTop() < aboutMe) {
         header.removeClass("backgroundWhite");
         header.removeClass("backgroundBlue");
@@ -48,7 +48,34 @@ $( window ).scroll(function() {
         header.removeClass("backgroundWhite");
         header.addClass("backgroundBlue");
     }
+
+    if ($(window).scrollTop() > 100)
+    {
+        $("#progressBarFillHtmlCss").addClass("fill80");
+        $("#progressBarFillHtmlCss>p").addClass("appear");
+        $("#progressBarFillHtmlCss").removeClass("empty");
+    }
+    if ($(window).scrollTop() > 165)
+    {
+        $("#progressBarFillJs").addClass("fill70");
+        $("#progressBarFillJs>p").addClass("appear");
+        $("#progressBarFillJs").removeClass("empty");
+    }
+    if ($(window).scrollTop() > 230)
+    {
+        $("#progressBarFillPhp").addClass("fill75");
+        $("#progressBarFillPhp>p").addClass("appear");
+        $("#progressBarFillPhp").removeClass("empty");
+    }
+    if ($(window).scrollTop() > 295)
+    {
+        $("#progressBarFillSql").addClass("fill60");
+        $("#progressBarFillSql>p").addClass("appear");
+        $("#progressBarFillSql").removeClass("empty");
+    }
+
 });
+
 
 $(function(){
     $.scrollIt();
@@ -60,8 +87,3 @@ $(function(){
     //     onPageChange: null,    // function(pageIndex) that is called when page is changed
     //     topOffset: 0           // offste (in px) for fixed top navigation
 });
-
-if ($(window).scrollTop() > 100)
-{
-    $("#progressBarFillHtmlCss").addClass("fill80")
-}
